aac9e045f3d08b79e8536cc0c8c27c5b0d191fef
Yakov Rakhamimov <jacobra19@gmail.com>
2020-06-05 13:14:48 +0300

    elm-format

--- src/Main.elm
+++ src/Main.elm
@@ -6,14 +6,11 @@ module Main exposing (..)
 --   https://guide.elm-lang.org/architecture/buttons.html
 --
 
-
 import Browser
-import Html exposing (Html, button, div, text,input,h1,header,main_,footer)
+import Html exposing (Html, button, div, footer, h1, header, input, main_, text)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick, onInput)
 import UUID exposing (UUID)
-
-import Html.Events exposing (onClick,onInput)
-
 
 
 
@@ -29,20 +26,21 @@ main =
 
 
 type alias Todo =
-    { id : String 
+    { id : String
     , label : String
     , isEditMode : Bool
     }
 
-type alias Model = {
-    todos: List Todo, 
-    newTodo: String}
 
+type alias Model =
+    { todos : List Todo
+    , newTodo : String
+    }
 
 
 init : Model
-init = { todos = [], newTodo = ""}
-
+init =
+    { todos = [], newTodo = "" }
 
 
 
@@ -50,39 +48,73 @@ init = { todos = [], newTodo = ""}
 
 
 type Msg
-  = HandleNewTodoChange String | AddTodo | DeleteTodo String | HandleEditBtn String | HandleTodoChange String String
+    = HandleNewTodoChange String
+    | AddTodo
+    | DeleteTodo String
+    | HandleEditBtn String
+    | HandleTodoChange String String
 
--- isFoundIdToRemove: Todo String -> Bool 
+
+
+-- isFoundIdToRemove: Todo String -> Bool
 -- isFoundIdToRemove todoModel id =
 --     todoModel.id == id
-    
+
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
         HandleNewTodoChange newTodoText ->
-            { model | newTodo = newTodoText}
+            { model | newTodo = newTodoText }
 
         AddTodo ->
             if model.newTodo == "" then
                 model
-            else 
-                { model | newTodo = "", todos = Todo model.newTodo model.newTodo False :: model.todos }
-        HandleEditBtn id->
-            { model | todos = List.map (\item -> if item.id == id then {item | isEditMode = True} else item ) model.todos }
-        
-        HandleTodoChange id todoText->
-            { model | todos = List.map (\item -> if item.id == id then {item | label = todoText} else item ) model.todos }
 
-        DeleteTodo id->
-            { model | todos = List.filter (\item -> item.id /= id) model.todos}
+            else
+                { model | newTodo = "", todos = Todo model.newTodo model.newTodo False :: model.todos }
+
+        HandleEditBtn id ->
+            { model
+                | todos =
+                    List.map
+                        (\item ->
+                            if item.id == id then
+                                { item | isEditMode = True }
+
+                            else
+                                item
+                        )
+                        model.todos
+            }
+
+        HandleTodoChange id todoText ->
+            { model
+                | todos =
+                    List.map
+                        (\item ->
+                            if item.id == id then
+                                { item | label = todoText }
+
+                            else
+                                item
+                        )
+                        model.todos
+            }
+
+        DeleteTodo id ->
+            { model | todos = List.filter (\item -> item.id /= id) model.todos }
 
 
 
 -- VIEW
+
+
 todoRowAttr : List (Html.Attribute msg)
-todoRowAttr = [ style "border-radius" "5px"
+todoRowAttr =
+    [ style "border-radius" "5px"
     , style "border" "1px solid lightgrey"
+
     -- , style "background-color" "red"
     , style "padding" "5px 15px"
     , style "display" "flex"
@@ -90,73 +122,87 @@ todoRowAttr = [ style "border-radius" "5px"
     , style "margin-bottom" "15px"
     ]
 
+
 rootAttr : List (Html.Attribute msg)
-rootAttr = [ 
-    style "display" "flex"
+rootAttr =
+    [ style "display" "flex"
     , style "align-items" "center"
     , style "flex-direction" "column"
     ]
 
-addTodoInputAttr : Model -> List (Html.Attribute Msg)
-addTodoInputAttr model = [
-    placeholder "Enter Todo"
-    , value model.newTodo
-    , onInput HandleNewTodoChange 
-    ,style "border" "1"
-    ,style "background" "whitesmoke"
-    ,style "border-radius" ".25rem"
-    ,style "padding" "5px"
-    ,style "color" "black"
-    ,style "margin-right" "15px"
+
+headerAttr : List (Html.Attribute msg)
+headerAttr =
+    [ style "max-width" "400px"
+    , style "width" "400px"
+    , style "display" "flex"
+    , style "align-items" "center"
+    , style "flex-direction" "column"
     ]
 
-addTodoBtnAttr: List (Html.Attribute Msg)
-addTodoBtnAttr = [
-    onClick AddTodo 
-    ,style "border" "1"
-    ,style "background" "whitesmoke"
-    ,style "border-radius" ".25rem"
-    ,style "padding" "5px"
-    ,style "color" "black"
+
+addTodoInputAttr : Model -> List (Html.Attribute Msg)
+addTodoInputAttr model =
+    [ placeholder "Enter Todo"
+    , value model.newTodo
+    , onInput HandleNewTodoChange
+    , style "border" "1"
+    , style "background" "whitesmoke"
+    , style "border-radius" ".25rem"
+    , style "padding" "5px"
+    , style "color" "black"
+    , style "margin-right" "15px"
     ]
+
+
+addTodoBtnAttr : List (Html.Attribute Msg)
+addTodoBtnAttr =
+    [ onClick AddTodo
+    , style "border" "1"
+    , style "background" "whitesmoke"
+    , style "border-radius" ".25rem"
+    , style "padding" "5px"
+    , style "color" "black"
+    ]
+
 
 viewTodoRow : Todo -> Html Msg
-viewTodoRow todoModel = 
+viewTodoRow todoModel =
+    div todoRowAttr
+        [ if todoModel.isEditMode then
+            input [ value todoModel.label, onInput (HandleTodoChange todoModel.id) ] []
 
-    div todoRowAttr [
-        if todoModel.isEditMode then
-            input [ value todoModel.label , onInput (HandleTodoChange todoModel.id) ] []
-        else
-            div [ ] [ text todoModel.label],
-        div [ style "display" "flex" ] [
-            button [ onClick (HandleEditBtn todoModel.id), style "margin-right" "15px" ] [text "Edit"]
-            , button [ onClick (DeleteTodo todoModel.id) ] [text "Delete"]
+          else
+            div [] [ text todoModel.label ]
+        , div [ style "display" "flex" ]
+            [ button [ onClick (HandleEditBtn todoModel.id), style "margin-right" "15px" ] [ text "Edit" ]
+            , button [ onClick (DeleteTodo todoModel.id) ] [ text "Delete" ]
+            ]
         ]
-    ]
 
-viewHeader: Html Msg
-viewHeader= 
-    header [] [
-        h1 [style "font-family" "Ubuntu, cursive"] [text "Todos App (Elm)"]
-    ]
 
-viewAddTodo: Model -> Html Msg
-viewAddTodo model = 
-    div [ style "margin-bottom" "15px", style "max-width" "400px"] [ 
-        input (addTodoInputAttr model) []
+viewHeader : Html Msg
+viewHeader =
+    header headerAttr
+        [ h1 [ style "font-family" "Ubuntu, cursive" ] [ text "Todos App (Elm)" ]
+        ]
+
+
+viewAddTodo : Model -> Html Msg
+viewAddTodo model =
+    div [ style "margin-bottom" "15px", style "max-width" "400px" ]
+        [ input (addTodoInputAttr model) []
         , button addTodoBtnAttr [ text "Add" ]
-    ]
+        ]
 
 
 view : Model -> Html Msg
 view model =
-    div rootAttr [
-        viewHeader,
-        main_ [ ] [
-            viewAddTodo model,
-
-            div [] (List.map viewTodoRow model.todos)
-
-        ],
-        footer [] []
-    ]
+    div rootAttr
+        [ viewHeader
+        , main_ []
+            [ viewAddTodo model
+            , div [] (List.map viewTodoRow model.todos)
+            ]
+        , footer [] []
+        ]
